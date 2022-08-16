509cfa0c08f70064fe4e7b1c7b7faf44f6d1a25e
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-08-16 22:48:56 +0000

    first commit

---
+++ pages/webinar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+    <HeroSection />
+    <ContentWebinar />
+    <Foote />
+  </div>
+</template>
+
+<script setup>
+import HeroSection from "../components/heroSection.vue";
+import ContentWebinar from "../components/content-webinar.vue";
+import Navbar from "../components/navbar.vue";
+import Foote from "../components/foote.vue";
+</script>
